864ce6cca5d54079ab32f53f97e8f9dd82a39c29
Ubuntu <ubuntu@ip-172-31-12-240.ec2.internal>
2021-07-22 14:53:12 +0000

    hello

--- mongodb/entrypoint/mongo-init.sh
+++ mongodb/entrypoint/mongo-init.sh
@@ -2,8 +2,9 @@
 
 echo "Creating suricata..."
 
-mongo --eval "db.createUser({user: 'lourdes', pwd: 'changeme',roles: [{role: 'dbOwner', db: 'suricata'}]});"
+mongo --eval "db.createUser({user: 'lourdes', pwd: 'changeme',roles: [{role: 'dbOwner', db: 'suricata'},{role: 'readWrite', db: 'test'}]});"
 mongo -u lourdes -p changeme --eval "db = db.getSiblingDB('suricata'); 
 db.createCollection('suricata');"
+
 
 echo "Finishing with suricata"
