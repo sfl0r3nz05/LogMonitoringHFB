f365913b3f6b1ce71e4de254af430a352113a459
Ubuntu <ubuntu@ip-172-31-23-238.ec2.internal>
2021-07-23 06:58:06 +0000

    23.07.21

--- mongodb/entrypoint/mongo-init.js
+++ mongodb/entrypoint/mongo-init.js
@@ -1,3 +1,14 @@
+db.createUser(
+    {
+        user: "lourdes",
+        pwd: "changeme",
+        roles: [
+            {
+                role: "readWrite",
+                db: "suricata"
+            }
+        ]
+    }
+);
 db = db.getSiblingDB('suricata');
-
 db.createCollection('suricata');
